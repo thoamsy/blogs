66c6a5e261ccaf043c58c4bd796ecfd5d9a9fff2
YangKui <c6swift@gmail.com>
2018-12-01 18:11:37 +0800

    refactor: 首页能够正常打开

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,71 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import './style.css'
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import './style.css';
 
-import Bio from '../components/Bio'
-import { rhythm } from '../utils/typography'
+import Layout from '../components/Layout';
+import Bio from '../components/Bio';
+import { rhythm } from '../utils/typography';
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
-
-    return (
-      <div>
-        <Helmet title={siteTitle} />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                className="blog-index"
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-}
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
+const BlogIndex = ({ location }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query IndexQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
-          frontmatter {
-            date(formatString: "YYYY/MM/DD")
-            title
+          allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+          ) {
+            edges {
+              node {
+                excerpt
+                fields {
+                  slug
+                }
+                frontmatter {
+                  date(formatString: "YYYY/MM/DD")
+                  title
+                }
+              }
+            }
           }
         }
-      }
-    }
-  }
-`
+      `}
+      render={data => {
+        const siteTitle = data.site.siteMetadata.title;
+        const posts = data.allMarkdownRemark.edges;
+        return (
+          <Layout location={location}>
+            <Helmet title={siteTitle} />
+            <Bio />
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug;
+              return (
+                <div key={node.fields.slug}>
+                  <h3
+                    className="blog-index"
+                    style={{
+                      marginBottom: rhythm(1 / 4),
+                    }}
+                  >
+                    <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                      {title}
+                    </Link>
+                  </h3>
+                  <small>{node.frontmatter.date}</small>
+                  <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                </div>
+              );
+            })}
+          </Layout>
+        );
+      }}
+    />
+  );
+};
+export default BlogIndex;
