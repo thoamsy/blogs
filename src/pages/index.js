ab64e99a55f5f3f99e0ad60bb6e6c754eeeec2c8
YangKui <c6swift@gmail.com>
2018-12-01 18:19:33 +0800

    feat: 调整代码逻辑

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import './style.css';
@@ -8,64 +8,57 @@ import Layout from '../components/Layout';
 import Bio from '../components/Bio';
 import { rhythm } from '../utils/typography';
 
-const BlogIndex = ({ location }) => {
+const BlogIndex = ({ location, data }) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
   return (
-    <StaticQuery
-      query={graphql`
-        query IndexQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          allMarkdownRemark(
-            sort: { fields: [frontmatter___date], order: DESC }
-          ) {
-            edges {
-              node {
-                excerpt
-                fields {
-                  slug
-                }
-                frontmatter {
-                  date(formatString: "YYYY/MM/DD")
-                  title
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        const siteTitle = data.site.siteMetadata.title;
-        const posts = data.allMarkdownRemark.edges;
+    <Layout location={location}>
+      <Helmet title={siteTitle} />
+      <Bio />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
         return (
-          <Layout location={location}>
-            <Helmet title={siteTitle} />
-            <Bio />
-            {posts.map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug;
-              return (
-                <div key={node.fields.slug}>
-                  <h3
-                    className="blog-index"
-                    style={{
-                      marginBottom: rhythm(1 / 4),
-                    }}
-                  >
-                    <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>{node.frontmatter.date}</small>
-                  <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                </div>
-              );
-            })}
-          </Layout>
+          <div key={node.fields.slug}>
+            <h3
+              className="blog-index"
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+            >
+              <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <small>{node.frontmatter.date}</small>
+            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
         );
-      }}
-    />
+      })}
+    </Layout>
   );
 };
 export default BlogIndex;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "YYYY/MM/DD")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,58 +1,59 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Bio from '../components/Bio';
+import Layout from '../components/Layout';
 import { rhythm, scale } from '../utils/typography';
 
-const BlogPostTemplate = ({ location }) => (
-  <StaticQuery
-    query={graphql`
-      query BlogPostBySlug($slug: String!) {
-        site {
-          siteMetadata {
-            title
-            author
-          }
-        }
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-          id
-          html
-          frontmatter {
-            title
-            date(formatString: "YYYY/MM/DD")
-          }
-        }
-      }
-    `}
-    render={({ markdownRemark: post, site }) => {
-      const siteTitle = site.siteMetadata.title;
+const BlogPostTemplate = ({
+  location,
+  data: { markdownRemark: post, site },
+}) => {
+  const siteTitle = site.siteMetadata.title;
 
-      return (
-        <Layout location={location}>
-          <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-          <h1>{post.frontmatter.title}</h1>
-          <p
-            style={{
-              ...scale(-1 / 5),
-              display: 'block',
-              marginBottom: rhythm(1),
-              marginTop: rhythm(-1),
-            }}
-          >
-            {post.frontmatter.date}
-          </p>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
-          />
-          <Bio />
-        </Layout>
-      );
-    }}
-  />
-);
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
+      <h1>{post.frontmatter.title}</h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: 'block',
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
+      >
+        {post.frontmatter.date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio />
+    </Layout>
+  );
+};
 
 export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+        date(formatString: "YYYY/MM/DD")
+      }
+    }
+  }
+`;
