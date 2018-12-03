675230062cb47de11559e97c1bd637ad95665291
Yang Kui <yangkui@youzan.com>
2018-12-03 14:16:21 +0800

    添加导航

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 import Bio from '../components/Bio';
 import Layout from '../components/Layout';
 import { rhythm, scale } from '../utils/typography';
 
+const PageNavigation = ({ previous, next }) => (
+  <ul
+    style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      listStyle: 'none',
+      padding: 0,
+    }}
+  >
+    <li>
+      {previous && (
+        <Link to={previous.fields.slug} rel="prev">
+          ← {previous.frontmatter.title}
+        </Link>
+      )}
+    </li>
+    <li>
+      {next && (
+        <Link to={next.fields.slug} rel="next">
+          {next.frontmatter.title} →
+              </Link>
+      )}
+    </li>
+  </ul>
+);
 const BlogPostTemplate = ({
   location,
+  pageContext,
   data: { markdownRemark: post, site },
 }) => {
   const siteTitle = site.siteMetadata.title;
+  console.log(post, pageContext)
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
+      <Helmet
+        title={`${post.frontmatter.title} | ${siteTitle}`} htmlAttributes={{ lang: 'zh-cn' }} />
       <h1>{post.frontmatter.title}</h1>
       <p
         style={{
