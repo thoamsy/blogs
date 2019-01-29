41964935a6e1982ba29cb1fb2104de5363936be2
YangKui <c6swift@gmail.com>
2019-01-29 23:44:28 +0800

    pref: 字体文件移除

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -33,7 +33,7 @@ const Layout = ({ location, title, children }) => {
     header = (
       <h3
         style={{
-          fontFamily: 'Montserrat, sans-serif',
+          fontFamily: 'sans-serif',
           marginTop: 0,
           marginBottom: rhythm(-1),
         }}
