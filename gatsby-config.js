16595c633e76ea8bff34c9da382f5fe89c85cb61
YangKui <c6swift@gmail.com>
2019-02-10 00:01:45 +0800

    config: 支持自动 autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
             },
           },
           'gatsby-remark-prismjs',
+          'gatsby-remark-autolink-headers',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
