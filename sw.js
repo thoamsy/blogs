98c87ea53beabbf21e1197e0e635eabcd0de6ff6
YangKui <c6swift@gmail.com>
2019-02-20 22:17:21 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1e7c9a82c364e9ea3e15.js"
+    "url": "webpack-runtime-e0b751c27ee632b8b974.js"
   },
   {
     "url": "app-8af1ca11e4e4dfa1aa1d.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "20798a1dc2b67e6da8749627968d4e87"
+    "revision": "4f387c032899270526d8fd99853aeecd"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
