faa34f1d96df7ab43afa5c71ebb1336e509c0999
YangKui <c6swift@gmail.com>
2019-05-19 18:48:49 +0800

    feat: 动画曲线继续调整

--- src/templates/components/Transition.jsx
+++ src/templates/components/Transition.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const Home = styled.div`
-  transition: all 0.4s cubic-bezier(0.215, 0.61, 0.355, 1);
   .page-enter & {
     opacity: 0;
     transform: translateX(-100%);
+    transition: all 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
   }
   .page-enter-active & {
     opacity: 1;
@@ -21,13 +21,17 @@ const Home = styled.div`
 `;
 
 const Detail = styled.div`
-  transition: all 0.4s cubic-bezier(0.215, 0.61, 0.355, 1);
   .page-enter & {
     transform: translateX(100%);
+    opacity: 0;
+    transition: all 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
   }
+
   .page-enter-active & {
+    opacity: 1;
     transform: translateX(0);
   }
+
   .page-exit & {
     transform: translateX(0);
     opacity: 1;
