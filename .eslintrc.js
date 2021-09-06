af9d2b412e3b7230757bca6da8aec9800c56bffe
thoamsy <c6swift@gmail.com>
2021-09-06 18:17:58 +0800

    part 1

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['react-app', 'plugin:jsx-a11y/recommended'],
-  globals: {
-    graphql: false,
-    globalThis: true,
-    __PATH_PREFIX__: false,
-  },
-};
